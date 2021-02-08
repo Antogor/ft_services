3248150b042b861d8779755feec05a4b2e0b044e
Antonio Garzon iniesta <agarzon-@c3r7s1.42madrid.com>
2021-02-08 13:30:15 +0100

    setup.sh

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -18,9 +18,14 @@ service mariadb start
 #/usr/bin/mysqld --user=root --bootstrap --verbose=0 < /tmp/init.sql
 
 #mysql -u root wordpress < wordpress.sql
+#mysql -u root --execute="CREATE DATABASE wordpress;"
+#mysql -u root --execute="CREATE USER 'agarzon'@'%' IDENTIFIED by 'agarzon'; GRANT ALL PRIVILEGES ON *.* TO 'agarzon'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
 mysql -u root --execute="CREATE DATABASE wordpress;"
-mysql -u root --execute="CREATE USER 'agarzon'@'%' IDENTIFIED by 'agarzon'; GRANT ALL PRIVILEGES ON *.* TO 'agarzon'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
-
+mysql -u root --execute="CREATE USER 'agarzon' IDENTIFIED by 'agarzon';"
+mysql -u root --execute="GRANT USAGE ON *.* TO 'agarzon'@'localhost' IDENTIFIED by 'agarzon';"
+mysql -u root --execute="GRANT USAGE ON *.* TO 'agarzon'@'%' IDENTIFIED by 'agarzon';"
+mysql -u root --execute="GRANT ALL privileges ON *.* TO 'agarzon'@'localhost'; USE wordpress;"
+mysql -u root --execute="GRANT ALL privileges ON *.* TO 'agarzon'@'%'; USE wordpress; FLUSH PRIVILEGES;"
 
 sleep infinity
 
