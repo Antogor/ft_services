5f93192d7703a06d5cc23c7cd094ec3cbad8dfeb
Antonio Garzon iniesta <agarzon-@c3r7s1.42madrid.com>
2021-02-08 14:51:47 +0100

    mysql work

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,33 +1,10 @@
 #!/bin/sh
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
-
 mariadb-install-db --bootstrap --user=root --datadir="/var/lib/mysql/"
-
-#mysql_secure_installation
-
-#mariadb-install-db -u root
-
 openrc
 touch /run/openrc/softlevel
-
 service mariadb start
-#/usr/bin/mysqld --user=root --bootstrap --verbose=0 < /tmp/init.sql
 
-#mysql -u root wordpress < wordpress.sql
-#mysql -u root --execute="CREATE DATABASE wordpress;"
-#mysql -u root --execute="CREATE USER 'agarzon'@'%' IDENTIFIED by 'agarzon'; GRANT ALL PRIVILEGES ON *.* TO 'agarzon'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
-mysql -u root --execute="CREATE DATABASE wordpress;"
-mysql -u root --execute="CREATE USER 'agarzon' IDENTIFIED by 'agarzon';"
-mysql -u root --execute="GRANT USAGE ON *.* TO 'agarzon'@'localhost' IDENTIFIED by 'agarzon';"
-mysql -u root --execute="GRANT USAGE ON *.* TO 'agarzon'@'%' IDENTIFIED by 'agarzon';"
-mysql -u root --execute="GRANT ALL privileges ON *.* TO 'agarzon'@'localhost'; USE wordpress;"
-mysql -u root --execute="GRANT ALL privileges ON *.* TO 'agarzon'@'%'; USE wordpress; FLUSH PRIVILEGES;"
+mysql -u root --password= < /tmp/init.sql
 
 sleep infinity
-
-#exec /usr/bin/mysqld --user=root --console
-#mariadb-safe --user=root --datadir="/var/lib/mysql/"
--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-#nginx -g 'daemon off;'
-#php-fpm7
 php -S 0.0.0.0:5050 -t /var/www/wordpress/
